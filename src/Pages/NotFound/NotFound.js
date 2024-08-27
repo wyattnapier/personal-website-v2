1e6bed13e55e1a241a2a23b31498305d613af3df
Wyatt <wnapier@bu.edu>
2024-08-26 22:08:29 -0400

    new page not found page and styling

---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typewriter } from "react-simple-typewriter";
+import "../../Global.css";
+import "./NotFound.css";
+
+function NotFound() {
+  return (
+    <div className="NotFound">
+      <h1>
+        <Typewriter
+          words={["", "Page not found. Go back."]}
+          loop={1} // keep looping forever!
+          cursor
+          typeSpeed={100}
+          delaySpeed={600}
+        />
+      </h1>
+      <p>
+        <Link to={"/"}>Home</Link>{" "}
+      </p>
+      <p>
+        <Link to={"/projects"}>Projects</Link>
+      </p>
+    </div>
+  );
+}
+
+export default NotFound;
